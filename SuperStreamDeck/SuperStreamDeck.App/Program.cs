a138f99fbcf59cbb46784097ce8b6b0f53e4a450
Nick Ganter <nickganter91@gmail.com>
2025-07-06 21:07:36 -0500

    Got the list panel looking the way i want it

--- SuperStreamDeck/SuperStreamDeck.App/Program.cs
+++ SuperStreamDeck/SuperStreamDeck.App/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Avalonia;
 using System;
+using Projektanker.Icons.Avalonia;
+using Projektanker.Icons.Avalonia.FontAwesome;
 
 namespace SuperStreamDeck.App;
 
@@ -14,8 +16,12 @@ sealed class Program
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
-        => AppBuilder.Configure<App>()
+    {
+        IconProvider.Current
+            .Register<FontAwesomeIconProvider>();
+        return AppBuilder.Configure<App>()
             .UsePlatformDetect()
             .WithInterFont()
             .LogToTrace();
+    }
 }
